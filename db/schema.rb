28bcd357f53838d0eca22f27868cb102de86b40d
Mei0506 <130352983+Mei0506@users.noreply.github.com>
2023-06-11 08:55:11 +0000

    [add]bookers

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_29_141459) do
+ActiveRecord::Schema.define(version: 2023_06_04_162243) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
